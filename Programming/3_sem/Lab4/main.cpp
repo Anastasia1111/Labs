74eb545ba6ab0ff66d5b115e0ef099b908d1164b
Stanislav Mayorov <minigamer97@gmail.com>
2016-09-25 15:58:26 +0600

    Добавил внятный main для лабы №4 по ООП
    Вместо вращения линия мерцает, что не ок.

--- Programming/3_sem/Lab4/main.cpp
+++ Programming/3_sem/Lab4/main.cpp
@@ -1,10 +1,31 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "line.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    system("PAUSE");
+    Line line;
+    
+    srand(time(0));
+    
+    int x = rand()%ScreenWidth + 1;
+    int y = rand()%ScreenHeight + 1;
+    int color = rand()%15 + 1;
+    int length = rand()%200 + 50;
+    
+    initwindow(ScreenWidth, ScreenHeight, "Lab 4", 200, 0);
+    
+    line.set(x, y, color, length);
+    line.Draw(color);
+    
+    delay(50);
+    
+    line.Rotate();
+    
+    system("PAUSE");	
+	closegraph();
     return EXIT_SUCCESS;
 }
