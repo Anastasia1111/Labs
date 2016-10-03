b123b22cc993fcc0a798ddbc29e7653e49065018
Monstruos <mongstruos@mail.ru>
2016-10-03 19:57:52 +0700

    Лаба 2-11 по ИТ
    Надеюсь, буду делать всегда по одной лабе в день.

---
+++ IT/lab 2-11/array.js
@@ -0,0 +1,51 @@
+array1 = new Array(0,0,0,0,0,0,0,0,0,0);
+array2 = new Array(0,0,0,0,0,0,0,0,0,0);
+PrintArray ();
+
+function randomInteger (min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
+}
+
+function PrintArray () {
+	
+	var index;
+
+	for (index = 0; index < 10; index++) {
+		document.getElementsByTagName('input')[index].value = array1[index];
+	}
+	
+	for (index = 12; index < 22;index++) {
+		document.getElementsByTagName('input')[index].value = array2[index - 12];
+	}
+}
+
+function RandomArray () {
+	var i;
+	
+	for(i = 0; i < 10; i++) {
+		array1[i] = randomInteger (100, 999);
+	}
+	PrintArray();
+}
+
+
+function readingValue (arrnum) {
+	
+	var val = document.getElementsByTagName('input')[arrnum].value;
+	
+	array1[arrnum] = val;
+
+	PrintArray();
+}
+
+function sorting() {
+	
+	for(var i = 0; i < 10; i++) {
+		array2[i] = array1[i];
+	}
+	
+	array2.sort();
+	PrintArray();
+}
