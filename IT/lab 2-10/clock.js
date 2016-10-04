2210413e2fc60bbf5c613ea23149a182c32ef3f2
Monstruos <mongstruos@mail.ru>
2016-10-04 20:49:15 +0700

    Сделана наполовину 2-12
    Сделал поиск элемента, не понимаю, что вообще требуется для таблицы.

--- IT/lab 2-10/clock.js
+++ IT/lab 2-10/clock.js
@@ -4,12 +4,8 @@
 function clockworking() {
 	
 	var now = new Date();
-	var curr_date = String(now.getDate());
-	var curr_month = String(now.getMonth() + 1);
 	var curr_year = String(now.getFullYear());
 	var curr_hour = String(now.getHours());
-	var curr_sec = String(now.getSeconds());
-	var curr_min = String(now.getMinutes());
 	
 	if (now.getHours() >= 12) {
 		curr_hour = String(parseInt(now.getHours() - 12));
@@ -26,37 +22,21 @@ function clockworking() {
 		document.h2.src = "LAB11/dg" + curr_hour.charAt(0) + ".gif"
 	}
 	
-	if(curr_min.length == 2) {
-		document.m1.src = "LAB11/dg" + curr_min.charAt(0) + ".gif"
-		document.m2.src = "LAB11/dg" + curr_min.charAt(1) + ".gif"
-	} else {
-		document.m1.src = "LAB11/dg0.gif"
-		document.m2.src = "LAB11/dg" + curr_min.charAt(0) + ".gif"
-	}
+		document.m1.src = "LAB11/dg" + parseInt(now.getMinutes()/10) + ".gif"
+		document.m2.src = "LAB11/dg" + now.getMinutes()%10 + ".gif"
+
 	
-	if(curr_sec.length == 2) {
-		document.s1.src = "LAB11/dg" + curr_sec.charAt(0) + ".gif"
-		document.s2.src = "LAB11/dg" + curr_sec.charAt(1) + ".gif"
-	} else {
-		document.s1.src = "LAB11/dg0.gif"
-		document.s2.src = "LAB11/dg" + curr_sec.charAt(0) + ".gif"
-	}
-	
-	if(curr_date.length == 2) {
-		document.d1.src = "LAB11/dg" + curr_date.charAt(0) + ".gif"
-		document.d2.src = "LAB11/dg" + curr_date.charAt(1) + ".gif"
-	} else {
-		document.d1.src = "LAB11/dg0.gif"
-		document.d2.src = "LAB11/dg" + curr_date.charAt(0) + ".gif"
-	}
-	
-	if(curr_month.length == 2) {
-		document.mo1.src = "LAB11/dg" + curr_month.charAt(0) + ".gif"
-		document.mo2.src = "LAB11/dg" + curr_month.charAt(1) + ".gif"
-	} else {
-		document.mo1.src = "LAB11/dg0.gif"
-		document.mo2.src = "LAB11/dg" + curr_month.charAt(0) + ".gif"
-	}
+		document.s1.src = "LAB11/dg" + parseInt(now.getSeconds()/10) + ".gif"
+		document.s2.src = "LAB11/dg" + now.getSeconds()%10 + ".gif"
+
+
+		document.d1.src = "LAB11/dg" + parseInt(now.getDate()/10) + ".gif"
+		document.d2.src = "LAB11/dg" + now.getDate()%10 + ".gif"
+
+
+		document.mo1.src = "LAB11/dg" + parseInt(now.getMonth()/10) + ".gif"
+		document.mo2.src = "LAB11/dg" + now.getMonth()%10 + ".gif"
+
 	
 	document.y1.src = "LAB11/dg" + curr_year.charAt(0) + ".gif"
 	document.y2.src = "LAB11/dg" + curr_year.charAt(1) + ".gif"
---
+++ IT/lab 2-12/array.js
@@ -0,0 +1,56 @@
+array1 = new Array(0,0,0,0,0,0,0,0,0);
+
+RandomArray ();
+PrintArray ();
+
+function randomInteger (min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
+}
+
+function RandomArray () {
+	var i;
+	
+	for(i = 0; i < 9; i++) {
+		array1[i] = randomInteger (100, 999);
+	}
+}
+
+function BinarySearch(t,A)
+{
+    var i = 0, j = 8, k;   
+
+    while (i <= j)
+    {  
+		k = Math.floor((i+j)/2);
+		if (t === A[k]) return k;
+		else if (t < A[k]) j = k-1;
+		else i = k+1;
+    }
+
+    return -1;
+}
+
+
+function PrintArray () {
+	
+	var index;
+
+	for (index = 0; index < 9; index++) {
+		document.getElementsByTagName('input')[index].value = array1[index];
+	}
+
+}
+
+
+function readingValue (arrnum) {
+	
+	var val = document.getElementsByTagName('input')[arrnum].value;
+
+	array1[arrnum] = val;
+	array1.sort();
+	PrintArray();
+	
+}
+
