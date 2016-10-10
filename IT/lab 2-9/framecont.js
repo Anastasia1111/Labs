d08fd026f90f6423857ab7d563c5d018d66d8f10
Monstruos <mongstruos@mail.ru>
2016-10-10 19:17:17 +0700

    доделал 2-9
    Мне сказали с window.open это сделать, я сделал!

--- IT/lab 2-9/framecont.js
+++
@@ -1,5 +0,0 @@
-function opening (fir, sec){
-
-            var ch = parent.document.frames.document.frames[0];
-            ch.document.frames[sec-1].document.getElementById('w'+ sec).src = fir;
-}
