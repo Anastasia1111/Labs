9b8e0dc5c012842ca0fa900842484e5e1d30670a
Stanislav Mayorov <minigamer97@gmail.com>
2016-11-04 13:27:26 +0700

    Добавил генератор рандомных чисел (РГЗ ИТ)

--- IT/RandomGZ/js/rand.js
+++ IT/RandomGZ/js/rand.js
@@ -1,4 +1,9 @@
 function getRandomInt(min, max)
 {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	if (isNaN(min) || isNaN(max))
+	{
+		alert('ֲגוהטעו צטפנû');
+	} else {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 }
