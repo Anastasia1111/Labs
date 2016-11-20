31c755c25ea8d300b098f0fbacf65d5bbb62f788
Monstruos <mongstruos@mail.ru>
2016-11-20 21:39:58 +0700

    Шеннон работает

--- SAOD/2_sem/lab_10-14/lab11.cpp
+++ SAOD/2_sem/lab_10-14/lab11.cpp
@@ -2,21 +2,25 @@
 #include <stdio.h>
 #include <math.h>
 
-void InsertSort(double* A, int n)
+void InsertSort(double* P, int *A, int n)
 {
 	int i, j;
 	double t1;
+	int t2;
 	for (i = 1; i < n; i++) {
-		t1 = A[i]; 
-		for (j = i - 1; (j >= 0) && (t1 >= A[j]); j--)
+		t1 = P[i]; 
+		t2 = A[i];
+		for (j = i - 1; (j >= 0) && (t1 >= P[j]); j--)
 		{
+			P[j+1] = P[j];
 			A[j+1] = A[j];
 		}
-		A[j + 1] = t1;
+		P[j+1] = t1;
+		A[j+1] = t2;
 	}
 }
 
-void p_calc_n_sort(double *P, int n, char *message)
+void p_calc_n_sort(double *P, int* A, int n, char *message)
 {
 	int k;
 	int i = 0;
@@ -28,20 +32,20 @@ void p_calc_n_sort(double *P, int n, char *message)
 	while (message[i] != 0)
 	{
 		j = message[i];
-		P[j] = P[j] + 1;
+		P[j-32] = P[j-32] + 1;
 		++i;
 	}
 	for (k = 0; k < n; k++)
 	{
 		P[k] = P[k]/(double)i;
 	}
-	InsertSort(P, n);
+	InsertSort(P, A, n);
 }
 
 main()
 {
 	int i, j;
-	int n = 128;
+	int n = 94;
 	int A[n];
 	double P[n];
 	double Q[n];
@@ -51,11 +55,11 @@ main()
 	iuygt np9eu5y-34t6[uspfojjhhj     ghjghjhj'pj9ytje7pku231glkjrhb dn wg;pu e8y n896y";
 	
 	int k;
-	for (k = 0; k <= n; k++)
+	for (k = 0; k < n; k++)
 	{
 		P[k] = 0.0;
 		L[k] = 0;
-		A[k] = k;
+		A[k] = k + 32;
 	}
 	/*
 	p_calc_n_sort(P, A, n, message);
@@ -90,13 +94,13 @@ main()
 		}
 	}*/
 	
-	p_calc_n_sort(P, n, message);
+	p_calc_n_sort(P, A, n, message);
 	
 	Q[0] = 0.0;
 	for(i = 1; i <= n; ++i)
 	{
 		Q[i] = Q[i-1] + P[i-1];
-		L[i] = (int) (-(log(P[i])/log(2)) + 1);
+		L[i] = (int) (-(log2(P[i])) + 1);
 		if(L[i] < 1) 
 			L[i] = 0;
 	}
@@ -116,7 +120,7 @@ main()
 	
 	for(i = 1; i < n; ++i)
 	{
-		printf("%i.  ", i);
+		printf("%4c.  ", A[i]);
 		for(j = 1; j <= L[i]; ++j)
 		{
 			printf("%c", C[i][j]);
