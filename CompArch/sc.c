0720b7ba7bea335178705b5696f3054151c2711c
Monstruos <mongstruos@mail.ru>
2017-02-13 20:35:15 +0700

    Наработки по ЭВМ
    Я окончательно завис на sc_reg функциями и их(ними )) ) параметрами. Мне
    нужна помощь в анализе того, что от нас требуют.

--- CompArch/sc.c
+++ CompArch/sc.c
@@ -3,10 +3,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MASK(x) (1<<(x-1))
+#define SET_FLAG_TRUE(x) |(1<<(x+24))
+#define SET_FLAG_FALSE(x) &(1<<(x+24))
 #define ramSize 100
 int RAM[ramSize];
-int REG;
+int REG;/* 0-14 = accumulator
+		*16-22 = command pointer
+		*24-28 = flag register
+		* 	24 (code 1):overfilling
+		*	25 (code 2):division on 0
+		*	26 (code 3):exit over limits of memory
+		* 	27 (code 4):step inpulses ignoring
+		* 	28 (code 5):wrong command
+		* */ 
 
 int sc_memoryInit ()
 {
@@ -14,29 +23,30 @@ int sc_memoryInit ()
 	{
 		RAM[i] = 0;
 	}
-	return 0;
 }
 
 int sc_memorySet (int address, int value)
 {
-	int errFlag = 1;
 	if(address > 0 && address < 99)
 	{
 		RAM[address] = value;
-		errFlag = 0;
+	} else {
+		sc_regSet(3, 1);
+		return 1;
 	}
-	return errFlag;
+	return 0;
 }
 
 int sc_memoryGet (int address, int *value)
 {
-	int errFlag = 1;
 	if(address > 0 && address < 99)
 	{
 		value = RAM+address;
-		errFlag = 0;
+	} else {
+		sc_regSet(3, 1);
+		return 1;
 	}
-	return errFlag;
+	return 0;
 }
 
 int sc_memorySave (char *filename)
@@ -70,14 +80,31 @@ int sc_regInit (void)
 	REG = 0;
 }
 
-int sc_regSet (int register, int value)
+int sc_regSet (int registr, int value)
 {
-	
+	if(registr > 0 && registr < 6 && value > 0 && value < 2)
+	{
+		if(value == 1)
+		{
+			REG = (REG) SET_FLAG_TRUE(registr);
+		} else {
+			REG = (REG) SET_FLAG_FALSE(registr);
+		}
+	} else {
+		return 1;
+	}
+	return 0;
 }
 
-int sc_regGet (int register, int *value)
+int sc_regGet (int registr, int *value)
 {
-	
+	if(registr > 0 && registr < 6)
+	{
+		
+	} else {
+		return 1;
+	}
+	return 0;
 }
 
 int sc_commandEncode (int command, int operand, int *value)
