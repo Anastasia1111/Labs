1cc1af90c5a171a3e0d973b51358939645743240
John Lenin <mongstruos@mail.ru>
2017-02-14 17:18:18 +0700

    Я исправил первые ошибки по ЭВМ (проверка настройки гита на ноуте)

--- CompArch/sc.c
+++ CompArch/sc.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define SET_FLAG_TRUE(x) |(1<<(x+24))
-#define SET_FLAG_FALSE(x) &(1<<(x+24))
+#define SET_FLAG_TRUE(x) |(1<<(x+23))
+#define SET_FLAG_FALSE(x) &(~(1<<(x+23)))
 #define ramSize 100
 int RAM[ramSize];
 int REG;/* 0-14 = accumulator
@@ -39,9 +39,9 @@ int sc_memorySet (int address, int value)
 
 int sc_memoryGet (int address, int *value)
 {
-	if(address > 0 && address < 99)
+	if(address >= 0 && address <= 99)
 	{
-		value = RAM+address;
+		*value = RAM[address];
 	} else {
 		sc_regSet(3, 1);
 		return 1;
