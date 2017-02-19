7735e1822892663d8f24c08c9e96455ce7ed8b31
Monstruos <mongstruos@mail.ru>
2017-02-19 17:37:13 +0700

    Наработки ЭВМ с пары
    Сегодня мне работать не дадут. В пн что-нибудь да будет.

--- CompArch/sc.c
+++ CompArch/sc.c
@@ -1,11 +1,23 @@
 #include "sc.h"
 
+#define ramSize 100
+int RAM[ramSize];
+int REG = 0;/* 0-14 = accumulator
+		*16-22 = command pointer
+		*24-28 = flag register
+		* 	24 (code 1):overfilling
+		*	25 (code 2):division on 0
+		*	26 (code 3):exit over limits of memory
+		* 	27 (code 4):step inpulses ignoring
+		* 	28 (code 5):wrong command
+		* */ 
 
-#define SET_FLAG_TRUE(x) |(1<<(x))
-#define SET_FLAG_FALSE(x) &(~(1<<(x)))
-#define GET_FLAG(val,x) (((val)>>(x))&1)
-#define DECODE_SEVENBIT_MASK &(~(~(0)<<7))
-#define FLAG_INIT &(~(0)>>8)
+#define SET_FLAG_TRUE(x) |(1<<(x + 24))
+#define SET_FLAG_FALSE(x) &(~(1<<(x + 24)))
+#define GET_FLAG(val,x) (((val)>>(x + 24))&1)
+#define GET_FLAG_COMMAND(val) (((val)>>14)&1)
+#define DECODE_SEVENBIT_MASK(x) (x)&(~(~(0)<<7))
+#define FLAG_INIT(x) (x)&(~(0)>>8)
 
 int sc_memoryInit ()
 {
@@ -69,7 +81,7 @@ int sc_memoryLoad (char *filename)
 
 int sc_regInit (void)
 {
-	REG = (REG) FLAG_INIT;
+	REG = FLAG_INIT(REG);
 	return 0;
 }
 
@@ -79,9 +91,9 @@ int sc_regSet (int registr, int value) // 0 <= registr <= 4
 	{
 		if(value == 1)
 		{
-			REG = (REG) SET_FLAG_TRUE(registr + 24);
+			REG = (REG) SET_FLAG_TRUE(registr);
 		} else {
-			REG = (REG) SET_FLAG_FALSE(registr + 24);
+			REG = (REG) SET_FLAG_FALSE(registr);
 		}
 	} else {
 		return 1;
@@ -93,7 +105,7 @@ int sc_regGet (int registr, int *value) // 0 <= registr <= 4
 {
 	if(registr >= 0 && registr < 5 && value != NULL)
 	{
-		*value = GET_FLAG(REG, registr + 24);
+		*value = GET_FLAG(REG, registr );
 	} else {
 		return 1;
 	}
@@ -113,7 +125,6 @@ int sc_commandEncode (int command, int operand, int *value)
 	{	
 		// encoding
 		*value = 0;
-		//*value = (*value) SET_FLAG_TRUE(14);
 		*value = (*value) | (command << (8-1));
 		*value = (*value) | (operand << (1-1));
 	} else {
@@ -124,26 +135,23 @@ int sc_commandEncode (int command, int operand, int *value)
 
 int sc_commandDecode (int value, int *command, int *operand)
 {
-	if(GET_FLAG(value, 14) == 1)
+	if(GET_FLAG_COMMAND(value) == 1)
 	{
-		REG = (REG) SET_FLAG_TRUE(5 + 24);
+		REG = (REG) SET_FLAG_TRUE(5);
 		return 1; //not command
 	}
 	
-	if(((value >> (8-1)) DECODE_SEVENBIT_MASK ) == 10 || ((value >> (8-1)) DECODE_SEVENBIT_MASK ) == 11 || 
-	((value >> (8-1)) DECODE_SEVENBIT_MASK ) == 20 || ((value >> (8-1)) DECODE_SEVENBIT_MASK ) == 21 || 
-	(((value >> (8-1)) DECODE_SEVENBIT_MASK ) >= 30 && ((value >> (8-1)) DECODE_SEVENBIT_MASK ) <= 33) || 
-	(((value >> (8-1)) DECODE_SEVENBIT_MASK ) >= 40 && ((value >> (8-1)) DECODE_SEVENBIT_MASK ) <= 76))
+	if((DECODE_SEVENBIT_MASK(value >> (8-1)) ) == 10 || ( DECODE_SEVENBIT_MASK(value >> (8-1)) ) == 11 || 
+	( DECODE_SEVENBIT_MASK(value >> (8-1)) ) == 20 || ( DECODE_SEVENBIT_MASK(value >> (8-1)) ) == 21 || 
+	(( DECODE_SEVENBIT_MASK(value >> (8-1)) ) >= 30 && ( DECODE_SEVENBIT_MASK(value >> (8-1)) ) <= 33) || 
+	(( DECODE_SEVENBIT_MASK(value >> (8-1)) ) >= 40 && ( DECODE_SEVENBIT_MASK(value >> (8-1)) ) <= 76))
 	{
 		*command = value >> (8-1);
 	} else {
-		REG = (REG) SET_FLAG_TRUE(5 + 24);
+		REG = (REG) SET_FLAG_TRUE(5);
 		return 2; // not existing command
 	}
-	
-	/*if(((value >> (1-1)) DECODE_SEVENBIT_MASK ) < 0 && ((value >> (1-1)) DECODE_SEVENBIT_MASK ) >= 256)
-		return 3; // not operand */ 
-	*operand = ((value >> (1-1)) DECODE_SEVENBIT_MASK );
+	*operand = ( DECODE_SEVENBIT_MASK(value >> (1-1)) );
 	return 0;
 }
 
