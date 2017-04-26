702e78d65ddf7cd684f79ed75e48a8b7b2cd5a5e
Stanislav Mayorov <minigamer97@gmail.com>
2017-04-26 15:12:29 +0700

    Положены основы практической работы
    Это просто пример перемещающегося объекта, а не что-то полезное.
    + Создана отдельная ветка, чтобы не засорять репозиторий (потом засорю,
    мухахах)

---
+++ Programming/4_sem (OVP)/SpaceSim/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    scene = new QGraphicsScene(this);
+    ui->graphicsView->setScene(scene);
+
+    // create our object and add it to the scene
+    item = new MyItem();
+    scene->addItem(item);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
