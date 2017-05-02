770110066f691d012089ec313763e248d584ae11
Monstruos <mongstruos@mail.ru>
2017-05-02 09:38:02 +0700

    Наработки с лабы

--- CompArch/Lab6/hdchs.c
+++ CompArch/Lab6/hdchs.c
@@ -2,42 +2,65 @@
 
 int g_lba2chs (tLBA input, tCHS *output)
 {
-	int mod[8] = {2, 4, 8, 16, 32, 64, 128, 255};
-	int buf = input / 63;
-	int ghead = buf / 1023;
-	int i;
-	for(i = 0; i < 8; ++i)
-	{
-		if(ghead <= mod[i])
-			break;
-	}
-	int ghead = mod[i];
-	int gcyl = buf / ghead;
-	int outcyl = (input / 63) / ghead;
-	int outhead = (input / 63) % ghead;
-	int outsec = input % 63 + 1;
-	*output = 0;
-	output += outcyl;
-	output <<= 6;
-	output += outsec;
-	output <<= 8;
-	output += head;
+	int cyl = (input.size / 63) / 16;
+	output->cylinder = cyl % 1024;
+	output->head = (input.size / 63) % 16;
+	output->sector = (input.size % 63) + 1;
+	return 0;
 };
-int g_lba2large (tLBA, tLARGE *);
-int g_lba2idechs (tLBA, tIDECHS *);
-int g_chs2large (tCHS, tLARGE *);
-int g_chs2lba (tCHS, tLBA *);
-int g_chs2idechs (tIDECHS, tLBA *);
-int g_large2chs (tLARGE, tCHS *);
+int g_lba2large (tLBA input, tLARGE *output)
+{
+	int cyl = (input.size / 63) / 256;
+	output->cylinder = cyl % 1024;
+	output->head = (input.size / 63) % 256;
+	output->sector = (input.size % 63) + 1;
+	return 0;
+};
+int g_lba2idechs (tLBA input, tIDECHS *output)
+{
+	int cyl = (input.size / 256) / 16;
+	output->cylinder = cyl % 65536;
+	output->head = (input.size / 256) % 16;
+	output->sector = (input.size % 256) + 1;
+	return 0;
+};
+int g_chs2large (tCHS input, tLARGE *output)
+{
+};
+int g_chs2lba (tCHS input, tLBA *output)
+{
+	output->size = input.cylinder * input.head * input.sector;
+	return 0;
+};
+int g_chs2idechs (tIDECHS input, tLBA *output)
+{
+	
+};
+int g_large2chs (tCHS input, tIDECHS output*);
 int g_large2idechs (tLARGE, tIDECHS *);
 int g_large2lba (tLARGE, tLBA *);
 int g_idechs2chs (tIDECHS, tCHS *);
-int g_idechs2lagre (tIDECHS, tLARGE *);
-int g_idechs2lba (tIDECHS, tLBA *);
-int a_lba2chs (tCHS geometry, tLBA, tCHS *);
+int g_idechs2large (tIDECHS, tLARGE *);
+int g_idechs2lba (tIDECHS input, tLBA *output)
+{
+	output->size = input.cylinder * input.head * input.sector;
+	return 0;
+};
+
+int a_lba2chs (tCHS geometry, tLBA input, tCHS *output)
+{
+	output->cylinder = input.size / geometry.sector / geometry.head;
+	output->head = (input.size / geometry.sector) % geometry.head;
+	output->sector = input.size % geometry.head + 1;
+	return 0;
+};
 int a_lba2large (tLARGE geometry, tLBA, tLARGE *);
 int a_lba2idechs (tIDECHS geometry, tLBA, tIDECHS *);
-int a_chs2lba (tCHS geometry, tCHS, tLBA *);
+int a_chs2lba (tCHS geometry, tCHS input, tLBA *output)
+{
+	output->size = (input.cylinder * geometry.head + input.head) * geometry.sector + input.sector + 1;
+	return 0;
+};
 int a_large2lba (tLARGE geometry, tLARGE, tLBA *);
 int a_idechs2lba (tIDECHS geometry, tIDECHS, tLBA *);
 int a_large2chs (tLARGE geometry1, tCHS geometry2, tLARGE, tCHS *);
