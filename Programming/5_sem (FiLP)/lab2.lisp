70f61ce03b9f2d0207b40b0b5491f08ab20e711d
Monstruos <mongstruos@mail.ru>
2017-09-26 19:24:26 +0700

    3 задание сделано (ФиЛП)
    Работает только при бинарной операции (типа "a операция b"). Думаю, это
    сойдет за готовое задание, поэтому я пошел делать ОС.

--- Programming/5_sem (FiLP)/lab2.lisp
+++ Programming/5_sem (FiLP)/lab2.lisp
@@ -15,7 +15,7 @@
 (adding 10 '(0 3 3 6 9))
 
 ;task #17
-(defun summar (l &optional s)
+(defun summar (l)
 	(cond
 		((equal l nil)
 			0
@@ -28,6 +28,14 @@
 (summar '(-2 3 2 5 -6 5 2 1 3))
 
 ;task #27
-(defun inpredfix (l &optional buf)
-	
+(defun inpredfix (l)
+	(cond
+		( (null l) 0 )
+		( (numberp l) l )
+		( (equal (cadr l) '*) (* (inpredfix (car l)) (inpredfix (caddr l))) )
+		( (equal (cadr l) '/) (/ (inpredfix (car l)) (inpredfix (caddr l))) )
+		( (equal (cadr l) '+) (+ (inpredfix (car l)) (inpredfix (caddr l))) )
+		( (equal (cadr l) '-) (- (inpredfix (car l)) (inpredfix (caddr l))) )
+	)
 )
+(inpredfix '((-8 + 10) * (12 / 3)))
