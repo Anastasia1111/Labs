4959d6e711cf226a6ef0cf0b87b4bd73a6bf937a
Stanislav Mayorov <minigamer97@gmail.com>
2017-09-28 19:31:06 +0700

    Переделал (?) лабу по АВС
    Я хз, вроде рабочий код должен быть, но я ничего не смыслю в этих ваших сопроцессорах

--- CompArch/sem2/lab1/fmin/lab1-3.cpp
+++ CompArch/sem2/lab1/fmin/lab1-3.cpp
@@ -9,7 +9,11 @@ int main(int argc, char** argv)
     for (int i=0; i<INT_MAX/100; ++i){
 		float f1 = i*1.0;
 		float f2 = i*1.5;
-		asm ("minss %1, %0" : "+x" (f1) : "x" (f2));
+		asm ("FCMPS s1, s2\n\t"
+		     "FMSTAT\n\t"
+		     "FCPYSGT s1, s2\n\t"
+		     : "s1" (f1)
+		     : "s2" (f2));
 	}
 	int end = clock();
 	printf("%d ms\n", end - start);
