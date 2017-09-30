03e5e0a035445b0e43d9507036debaeecb0427f7
Stanislav Mayorov <minigamer97@gmail.com>
2017-09-30 18:04:07 +0700

    Наработки за 30.09
    АВС (моя версия)

--- CompArch/sem2/lab1/fmin/lab1-3.cpp
+++ CompArch/sem2/lab1/fmin/lab1-3.cpp
@@ -9,11 +9,18 @@ int main(int argc, char** argv)
     for (int i=0; i<INT_MAX/100; ++i){
 		float f1 = i*1.0;
 		float f2 = i*1.5;
-		asm ("FCMPS s1, s2\n\t"
-		     "FMSTAT\n\t"
-		     "FCPYSGT s1, s2\n\t"
-		     : "s1" (f1)
-		     : "s2" (f2));
+		float res;
+		asm (
+			"FCMPS %[f1], %[f2]\n\t"
+		     	"FMSTAT\n\t"
+			"FCPYS %[res], %[f1]\n\t"
+			"BLS .END\n\t"
+		     	"FCPYS %[res], %[f2]\n\t"
+			".END:\n\t"
+		     	: [res] "=w" (res)
+		     	: [f1] "w" (f1)
+			, [f2] "w" (f2)
+		);
 	}
 	int end = clock();
 	printf("%d ms\n", end - start);
