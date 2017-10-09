a29c5ecd2126b6f8d6b74ec05091ef31e2387a24
Stanislav Mayorov <minigamer97@gmail.com>
2017-10-09 22:33:43 +0700

    Окончательно угробил быстрое умножение

--- Ruban/2_sem/Lab4/FNick/src/ru/labs/fnick/Multiplication.java
+++ Ruban/2_sem/Lab4/FNick/src/ru/labs/fnick/Multiplication.java
@@ -5,67 +5,8 @@ final public class Multiplication {
 
     private Multiplication(){}
 
-    static private long recursiveFastStep(int x, int y, int n)
+    static private long recursiveFastStep(int x, int y)
     {
-        if (n <= 1)
-            return x & y;
-
-        long res;
-        if (n % 2 != 0)
-        {
-            int a1 = x >> (n - 1),
-                a2 = x & (~(1 << (n - 1))),
-                b1 = y >> (n - 1),
-                b2 = y & (~(1 << (n - 1)));
-            res = (a1 & b1) << (2 * (n - 1));
-            if (a1 == 0)
-            {
-                res += a2 << (n - 1);
-            } else {
-                if (b1 == 0)
-                {
-                    res += b2 << (n - 1);
-                } else {
-                    int sum = a2 + b2;
-                    if (a2 != 0 && b2 != 0)
-                        lastT += (n - 1) / 2;
-                    res += sum << (n - 1);
-                }
-            }
-            res += recursiveFastStep(a2, b2, (n - 1));
-        } else {
-            int k = n / 2;
-            int a = x >> k;
-            int b = x & ((1 << k) - 1);
-            int c = y >> k;
-            int d = y & ((1 << k) - 1);
-            int apb = a + b;
-            if (a != 0 && b != 0)
-                lastT += k;
-            int cpd = c + d;
-            if (c != 0 && d != 0)
-                lastT += k;
-            long u;
-            if (apb >= (1 << k) || cpd >= (1 << k))
-            {
-                u = recursiveFastStep(apb, cpd, k + 1);
-            } else {
-                u = recursiveFastStep(apb, cpd, k);
-            }
-            long v = recursiveFastStep(a, c, k);
-            long w = recursiveFastStep(b, d, k);
-            res = (v << n) + ((u - v - w) << k) + w;
-            if (u != 0 && v != 0)
-                lastT += n;
-            if ((u - v) != 0 && w != 0)
-                lastT += n;
-        }
-        return res;
-    }
-
-    static public long fast(int x, int y)
-    {
-        lastT = 0;
         int n = 0;
         if (x > y)
             while ((x >> n) != 0)
@@ -73,7 +14,49 @@ final public class Multiplication {
         else
             while ((y >> n) != 0)
                 ++n;
-        return recursiveFastStep(x, y, n);
+        long res;
+        if (n <= 1)
+            return x * y;
+
+        int k = n / 2;
+        int a = x >> k;
+        int b = x & ((1 << k) - 1);
+        int c = y >> k;
+        int d = y & ((1 << k) - 1);
+        long u;
+        int sum1 = a + b;
+        int sum2 = c + d;
+        if (sum1 >= (1 << k) || sum2 >= (1 << k))
+        {
+            int a1 = sum1 >> k;
+            int b1 = sum2 >> k;
+            int a2 = sum1 & ((1 << k) - 1);
+            int b2 = sum2 & ((1 << k) - 1);
+            u = (a1 * a2) << (2 * k);
+            if (a1 != 0)
+            {
+                if (b1 != 0)
+                {
+                    u += (b2 + a2) << k;
+                } else {
+                    u += b2 << k;
+                }
+            } else {
+                u += a2 << k;
+            }
+            u += recursiveFastStep(a2, b2);
+        } else {
+            u = recursiveFastStep(sum1, sum2);
+        }
+        long v = recursiveFastStep(a, c);
+        long w = recursiveFastStep(b, d);
+        return (v << (2 * k)) + ((u - v - w) << k) + w;
+    }
+
+    static public long fast(int x, int y)
+    {
+        lastT = 0;
+        return recursiveFastStep(x, y);
     }
 
     static public long column(int x, int y)
@@ -105,18 +88,12 @@ final public class Multiplication {
             }
         return sum;
     }
-    private static int T(int n)
-    {
-        if(n == 1) return 1;
-        return 3*T(n/2) + (n % 2 != 0 ? 4*n : 3*n);
-    }
 
     public static void test()
     {
-        int x = 3869;//65535;
-        int y = 8324;//65535;
+        int x = 20;
+        int y = 30;
         System.out.println(x + " * " + y + " = " + Multiplication.column(x, y) + "\nT = " + lastT);
         System.out.println(x + " * " + y + " = " + Multiplication.fast(x, y) + "\nT = " + lastT);
-        System.out.println("Theoretical fast T = " + T(16));
     }
 }
