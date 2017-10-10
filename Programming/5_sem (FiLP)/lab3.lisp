ef3c188cac222f056815c2d73a1ba3a921a95e12
Stanislav Mayorov <minigamer97@gmail.com>
2017-10-10 19:01:24 +0700

    Сделал 3-ю лабу по ФиЛПу

--- Programming/5_sem (FiLP)/lab3.lisp
+++ Programming/5_sem (FiLP)/lab3.lisp
@@ -17,20 +17,32 @@
 ;task #6
 (defun compl (l1 l2)
 	(cond
-		((and (null l1) (null l2))
-			t
+		((or (null l1) (null l2))
+			l1
 		)
 		((member (car l1) l2)
-			(equ (remove (car l1) l1) (remove (car l1) l2))
+			(compl (remove (car l1) l1) (remove (car l1) l2))
 		)
 		(t
-			nil
+			(cons (car l1) (compl (remove (car l1) l1) (remove (car l1) l2)))
 		)
 	)
 )
-(equ '(0 9 3 6 3) '(0 3 3 6 9))
+(compl '(0 12 9 3 6 3 4 10 (5 6)) '(0 3 10 3 6 9 5 6))
 
 ;task #9
-(defun 
+(defun func (p l)
+	(cond
+		((null l)
+			l
+		)
+		((funcall p (car l))
+			(append '(*)  (list (car l)) (func p (cdr l)))
+		)
+		(t
+			(append (list (car l)) (func p (cdr l)))
+		)
+	)
 )
-(inpredfix '(8 * 10))
+(func (lambda (x) (>= x 0)) '(0 9 -3 6 -3))
+(func 'evenp '(0 9 3 6 3))
