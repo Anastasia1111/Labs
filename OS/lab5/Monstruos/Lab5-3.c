658a12b8e6a2b2ac059992fc81516c95913cc427
Monstruos <mongstruos@mail.ru>
2017-10-18 17:32:18 +0700

    "Моя" 3 лаба по ОСям

--- OS/lab5/Monstruos/Lab5-3.c
+++ OS/lab5/Monstruos/Lab5-3.c
@@ -1,5 +1,37 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+struct msg
+{
+	long mtype;
+	char mess[255];
+};
+
 int main(int argc, char const *argv[])
 {
-	
+	key_t key = ftok("./text", 42L);
+	int msgid = msgget(key, IPC_CREAT);
+	struct msg tmp;
+	tmp.mtype = 46L;
+	switch(fork())
+	{
+		case 0:
+			/*Child*/
+			msgrcv(msgid, &tmp, sizeof(tmp), 46L, 0);
+			printf("%s\n", tmp.mess);
+			break;
+			
+		default:
+			/*Parent*/			
+			strcpy(tmp.mess, "Hello World!");
+			msgsnd(msgid, (void*)&tmp, sizeof(tmp), 0);
+			wait(0);
+			break;
+	}
 	return 0;
 }
