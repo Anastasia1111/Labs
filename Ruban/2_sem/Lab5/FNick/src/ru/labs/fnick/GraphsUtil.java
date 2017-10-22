6366c6cb0a1e855cafb1ab4240ab61ac392b2a6e
Stanislav Mayorov <minigamer97@gmail.com>
2017-10-22 19:19:07 +0700

    Доделал (?) лабы по графам (Рубан)
    Вместо своих костылей подключил нормальный графопостроитель, а также добавил вывод кратчайших путей

--- Ruban/2_sem/Lab5/FNick/src/ru/labs/fnick/GraphsUtil.java
+++ Ruban/2_sem/Lab5/FNick/src/ru/labs/fnick/GraphsUtil.java
@@ -1,5 +1,7 @@
 package ru.labs.fnick;
 
+import org.graphstream.graph.Edge;
+
 import java.util.*;
 
 final public class GraphsUtil {
@@ -7,11 +9,11 @@ final public class GraphsUtil {
 
     private GraphsUtil(){}
 
-    public static Graph spanningTree(Graph graph)
+    public static GraphAdapter spanningTree(GraphAdapter graph)
     {
         lastT = 0;
 
-        ArrayList<Graph.Edge> edges = graph.getEdges();
+        ArrayList<Edge> edges = graph.getEdges();
         ArrayList<String> vertices = graph.getVertices();
         int k = edges.size();
         int n = vertices.size();
@@ -21,29 +23,29 @@ final public class GraphsUtil {
             component.put(v, ++i);
         int graphComponent = 0;
 
-        Graph tree = (graph instanceof UndirectedGraph) ? new UndirectedGraph() : new DirectedGraph();
+        GraphAdapter tree = new GraphAdapter(graph.isDirected());
 
-        Collections.sort(edges, new Comparator<Graph.Edge>() {
+        Collections.sort(edges, new Comparator<Edge>() {
             @Override
-            public int compare(Graph.Edge o1, Graph.Edge o2) {
-                return o1.weight.compareTo(o2.weight);
+            public int compare(Edge o1, Edge o2) {
+                return ((Integer)o1.getAttribute("weight")).compareTo(o2.getAttribute("weight"));
             }
         });
         lastT += k * (int) Math.ceil(Math.log(k)/Math.log(2));
 
-        Graph.Edge edge = edges.get(0);
-        component.replace(edge.v1, graphComponent);
-        component.replace(edge.v2, graphComponent);
+        Edge edge = edges.get(0);
+        component.replace(edge.getNode0().toString(), graphComponent);
+        component.replace(edge.getNode1().toString(), graphComponent);
         lastT += 2;
-        tree.addEdge(edge);
+        tree.addEdge(edge.getNode0().toString(), edge.getNode1().toString(), edge.getAttribute("weight"));
         edges.remove(0);
         while(tree.getEdges().size() != (n - 1))
         {
             if (edges.size() == 0)
                 return null;
             edge = edges.get(0);
-            int c1 = component.get(edge.v1);
-            int c2 = component.get(edge.v2);
+            int c1 = component.get(edge.getNode0().toString());
+            int c2 = component.get(edge.getNode1().toString());
             if (c1 != c2)
             {
                 int minComp = Math.min(c1, c2);
@@ -53,7 +55,7 @@ final public class GraphsUtil {
                         entry.setValue(minComp);
                         ++lastT;
                     }
-                tree.addEdge(edge);
+                tree.addEdge(edge.getNode0().toString(), edge.getNode1().toString(), edge.getAttribute("weight"));
             }
             ++lastT;
             edges.remove(0);
@@ -61,7 +63,7 @@ final public class GraphsUtil {
         return tree;
     }
 
-    public static int[] shortestPathFB(Graph graph)
+    public static String[] shortestPathFB(GraphAdapter graph)
     {
         lastT = 0;
         boolean lock = false;
@@ -76,12 +78,23 @@ final public class GraphsUtil {
                 C[i][j] = inf;
             C[i][i] = 0;
         }
-        for (Graph.Edge e : graph.getEdges())
-            C[vertices.indexOf(e.v1)][vertices.indexOf(e.v2)] = C[vertices.indexOf(e.v2)][vertices.indexOf(e.v1)] = e.weight;
+        if (graph.isDirected())
+        {
+            for (Edge e : graph.getEdges())
+                C[vertices.indexOf(e.getNode0().toString())][vertices.indexOf(e.getNode1().toString())] = e.getAttribute("weight");
+        } else {
+            for (Edge e : graph.getEdges())
+                C[vertices.indexOf(e.getNode0().toString())][vertices.indexOf(e.getNode1().toString())] = C[vertices.indexOf(e.getNode1().toString())][vertices.indexOf(e.getNode0().toString())] = e.getAttribute("weight");
+        }
 
         int[] D = new int[n];
+        String[] path = new String[n];
+        path[0] = vertices.get(0);
         for (int i = 1; i < n; ++i)
+        {
             D[i] = inf;
+            path[i] = vertices.get(0);
+        }
         int[] Dprev = D.clone();
 
         for (int l = 0; l < n; ++l)
@@ -90,25 +103,32 @@ final public class GraphsUtil {
             for (int i = 1; i < n; ++i)
             {
                 int min = inf;
+                int mini = -1;
                 for (int j = 0; j < n; ++j)
                 {
                     if (Dprev[j] != inf && C[j][i] != inf)
+                    {
                         min = Math.min(min, Dprev[j] + C[j][i]);
+                        if (min == Dprev[j] + C[j][i]) mini = j;
+                    }
                     ++lastT;
                 }
                 if (D[i] != min)
                 {
                     D[i] = min;
+                    path[i] = path[mini] + "->" + vertices.get(i);
                     lock = false;
                 }
             }
             Dprev = D.clone();
             if (lock) break;
         }
-        return D;
+        for (int i = 0; i < n; ++i)
+            path[i] += (" for " + D[i]);
+        return path;
     }
 
-    public static int[] shortestPathDijkstra(Graph graph)
+    public static String[] shortestPathDijkstra(GraphAdapter graph)
     {
         lastT = 0;
         ArrayList<String> V = graph.getVertices();
@@ -123,12 +143,23 @@ final public class GraphsUtil {
                 C[i][j] = inf;
             C[i][i] = 0;
         }
-        for (Graph.Edge e : graph.getEdges())
-            C[vertices.indexOf(e.v1)][vertices.indexOf(e.v2)] = C[vertices.indexOf(e.v2)][vertices.indexOf(e.v1)] = e.weight;
+        if (graph.isDirected())
+        {
+            for (Edge e : graph.getEdges())
+                C[vertices.indexOf(e.getNode0().toString())][vertices.indexOf(e.getNode1().toString())] = e.getAttribute("weight");
+        } else {
+            for (Edge e : graph.getEdges())
+                C[vertices.indexOf(e.getNode0().toString())][vertices.indexOf(e.getNode1().toString())] = C[vertices.indexOf(e.getNode1().toString())][vertices.indexOf(e.getNode0().toString())] = e.getAttribute("weight");
+        }
 
         int[] D = new int[n];
+        String[] path = new String[n];
+        path[0] = vertices.get(0);
         for (int i = 1; i < n; ++i)
+        {
             D[i] = C[0][i];
+            path[i] = D[i] != inf ? vertices.get(0) + "->" + vertices.get(i) : "";
+        }
 
         ArrayList<String> S = new ArrayList<String>();
         S.add(V.get(0));
@@ -147,45 +178,69 @@ final public class GraphsUtil {
                 int i = vertices.indexOf(v);
                 int j = vertices.indexOf(w);
                 if (D[j] != inf && C[j][i] != inf)
+                {
                     D[i] = Math.min(D[i], D[j] + C[j][i]);
+                    if (D[i] == D[j] + C[j][i]) path[i] = path[j] + "->" + vertices.get(i);
+                }
                 lastT += 2;
             }
         }
-        return D;
+        for (int i = 0; i < n; ++i)
+            path[i] += (" for " + D[i]);
+        return path;
     }
 
     public static void test()
     {
-        ArrayList<Graph.Edge> edges = new ArrayList<>();
-        edges.add(new Graph.Edge("1",  "2",  1));
-        edges.add(new Graph.Edge("2",  "3",  1));
-        edges.add(new Graph.Edge("3",  "7",  3));
-        edges.add(new Graph.Edge("7",  "8",  1));
-        edges.add(new Graph.Edge("8",  "9",  3));
-        edges.add(new Graph.Edge("9",  "10",  5));
-        edges.add(new Graph.Edge("10",  "4",  2));
-        edges.add(new Graph.Edge("4",  "1",  1));
-        edges.add(new Graph.Edge("1",  "5",  8));
-        edges.add(new Graph.Edge("5",  "2",  4));
-        edges.add(new Graph.Edge("2",  "6",  2));
-        edges.add(new Graph.Edge("6",  "3",  1));
-        edges.add(new Graph.Edge("6",  "7",  2));
-        edges.add(new Graph.Edge("8",  "6",  1));
-        edges.add(new Graph.Edge("6",  "9",  2));
-        edges.add(new Graph.Edge("9",  "5",  1));
-        edges.add(new Graph.Edge("5",  "10",  1));
-        edges.add(new Graph.Edge("4",  "5",  7));
-        edges.add(new Graph.Edge("5",  "6",  3));
-        DirectedGraph graph = new DirectedGraph(edges);
+        boolean directed = false;
+        GraphAdapter graph = new GraphAdapter(directed);
+        /*graph.addEdge("1",  "2",  1);
+        graph.addEdge("2",  "3",  1);
+        graph.addEdge("3",  "7",  3);
+        graph.addEdge("7",  "8",  1);
+        graph.addEdge("8",  "9",  3);
+        graph.addEdge("9",  "10",  5);
+        graph.addEdge("10",  "4",  2);
+        graph.addEdge("4",  "1",  1);
+        graph.addEdge("1",  "5",  8);
+        graph.addEdge("5",  "2",  4);
+        graph.addEdge("2",  "6",  2);
+        graph.addEdge("6",  "3",  1);
+        graph.addEdge("6",  "7",  2);
+        graph.addEdge("8",  "6",  1);
+        graph.addEdge("6",  "9",  2);
+        graph.addEdge("9",  "5",  1);
+        graph.addEdge("5",  "10",  1);
+        graph.addEdge("4",  "5",  7);
+        graph.addEdge("5",  "6",  3);*/
+        graph.addEdge("A", "B", 1);
+        graph.addEdge("A", "D", 2);
+        graph.addEdge("A", "G", 3);
+        graph.addEdge("A", "F", 15);
+        graph.addEdge("B", "D", 1);
+        graph.addEdge("D", "C", 5);
+        graph.addEdge("C", "B", 6);
+        graph.addEdge("C", "F", 2);
+        graph.addEdge("E", "F", 4);
+        graph.addEdge("K", "F", 2);
+        graph.addEdge("E", "D", 7);
+        graph.addEdge("K", "E", 3);
+        graph.addEdge("K", "D", 4);
+        graph.addEdge("G", "K", 1);
+        graph.addEdge("D", "G", 1);
         System.out.println("Graph:\n" + graph);
+        graph.display();
 
-        DirectedGraph tree = (DirectedGraph) GraphsUtil.spanningTree(graph);
+        GraphAdapter tree = GraphsUtil.spanningTree(graph);
         if (tree == null)
+        {
             System.out.println("Graph is disconnected");
-        else
+        } else {
             System.out.println("Tree:\n" + tree + "Weight: " + tree.getWeight() + "\nT = " + lastT);
+            tree.display();
+        }
 
-        int[] path = GraphsUtil.shortestPathFB(graph);
+        String[] path = GraphsUtil.shortestPathFB(graph);
         int index = 0;
         System.out.println("Shortest path from " + graph.getVertices().get(0) + ":");
         for (String v : graph.getVertices())
