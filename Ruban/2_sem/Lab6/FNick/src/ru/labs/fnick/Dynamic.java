3e1d289d67363dd32935538b4571c2001085b6a2
Stanislav Mayorov <minigamer97@gmail.com>
2017-10-23 19:28:31 +0700

    Доделал (?) динамику
    Хз, что там верно, а что нет, но на примерах из методички работает

--- Ruban/2_sem/Lab6/FNick/src/ru/labs/fnick/Dynamic.java
+++ Ruban/2_sem/Lab6/FNick/src/ru/labs/fnick/Dynamic.java
@@ -1,44 +1,92 @@
 package ru.labs.fnick;
 
-import java.util.Date;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 final public class Dynamic {
     public static long lastT = 0;
 
     private Dynamic(){}
 
-    public static int Knapsack(int[] m, int[] c, int M)
+    public static String knapsack(int[] m, int[] c, int M)
     {
+        lastT = 0;
         int n = c.length;
 
-        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        ArrayList<Pair<Integer, Integer>> items = new ArrayList<>();
         for (int i = 0; i < n; ++i)
-            map.put(m[i], c[i]);
+            items.add(new Pair<>(m[i], c[i]));
+        Collections.sort(items, new Comparator<Pair<Integer, Integer>>() {
+            @Override
+            public int compare(Pair<Integer, Integer> o1, Pair<Integer, Integer> o2) {
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
 
         int[] f = new int[M + 1];
+        String[] set = new String[M + 1];
         for (int i = 0; i <= M; ++i)
         {
             f[i] = 0;
-            for (Map.Entry<Integer, Integer> entry : map.entrySet())
+            set[i] = "Knapsack:[";
+            int maxj = -1;
+            for (int j = 0; j < n; ++j)
             {
-                if (i - entry.getKey() < 0) break;
-                f[i] = Math.max(f[i], f[i - entry.getKey()] + entry.getValue());
+                int mj = items.get(j).getKey();
+                int cj = items.get(j).getValue();
+                if (i - mj < 0) break;
+                f[i] = Math.max(f[i], f[i - mj] + cj);
+                if (f[i] == (f[i - mj] + cj)) maxj = j;
                 ++lastT;
             }
+            if (maxj != -1) set[i] = set[i - items.get(maxj).getKey()] + items.get(maxj).getKey() + "(" + items.get(maxj).getValue() + ") ";
         }
+        set[M] = set[M].trim() + "] C = " + f[M];
 
-        return f[M];
+        return set[M];
+    }
+
+    public static String matrix(int[] r)
+    {
+        lastT = 0;
+        int n = r.length - 1;
+        int[][] f = new int[n + 1][n + 1];
+        String[][] bkt = new String[n + 1][n + 1];
+        for (int i = 1; i <= n; ++i)
+        {
+            f[i][i] = 0;
+            bkt[i][i] = ("M" + i);
+        }
+        for (int t = 1; t < n; ++t)
+            for (int k = 1; k <= (n - t); ++k)
+            {
+                int min = Integer.MAX_VALUE;
+                int minj = 0;
+                for (int j = k; j < (k + t); ++j)
+                {
+                    int prevmin = min;
+                    min = Math.min(min, f[k][j] + f[j + 1][k + t] + r[k - 1] * r[j] * r[k + t]);
+                    if (min != prevmin) minj = j;
+                    ++lastT;
+                }
+                bkt[k][k + t] = "(" + bkt[k][minj] + ")(" + bkt[minj + 1][k + t] + ")";
+                f[k][k + t] = min;
+            }
+        bkt[1][n] += (" for " + f[1][n]);
+        return bkt[1][n];
     }
 
     public static void test()
     {
-        int[] m = {8, 5, 3};
-        int[] c = {23, 14, 8};
+        int[] m = {8, 5, 2};
+        int[] c = {23, 1, 1};
         int M = 13;
-        int C = Dynamic.Knapsack(m, c, 13);
-        System.out.println("F(" + M + ") = " + C + "\nT = " + lastT);
+        System.out.println("F(" + M + ") = " + Dynamic.knapsack(m, c, M) + "\nT = " + lastT);
+
+        int[] r = {10, 20, 50, 1, 100};
+        System.out.println(matrix(r) + "\nT = " + lastT);
     }
 }
