b0df1da02e3337f502d630654f21666047d6301a
Monstruos <mongstruos@mail.ru>
2017-10-24 17:51:14 +0700

    4 лаба

--- Programming/5_sem (FiLP)/lab4.pl
+++ Programming/5_sem (FiLP)/lab4.pl
@@ -17,12 +17,13 @@ woman(mary).
 woman(liz).
 woman(ann).
 % Родственники:
-diff(X,Y):- X=Y,!, fail; true.
+diff(X,Y):- X\==Y.
+diff(X,Y,Z):- X\==Y, Y\==Z, X\==Z.
 father(X,Y):-parent(X,Y),man(X).
 mother(X,Y):-parent(X,Y),woman(X).
 brother(X,Y):-man(X),parent(Z,X),parent(Z,Y), diff(X,Y).
 sister(X,Y):-parent(Z,X),parent(Z,Y), woman(X), diff(X,Y).
 grandchilds(X, Y):-parent(Y,Z),parent(Z,X).
 aunt(X,Y):-parent(Z,Y),sister(X, Z).
-has_2_childs(X):-parent(X,Y),parent(X,Z),diff(Y,Z).
+has_2_childs(X):-parent(X,Y),parent(X,Z),diff(Y,Z),not(parent(X,W)), diff(W,Y,Z).
 successor(X):-parent(X,Y),man(Y),man(X).
