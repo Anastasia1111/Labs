9c33363d06f48b7cc2b7e2b42eea95ee5f39a92a
Monstruos <mongstruos@mail.ru>
2017-10-29 17:53:13 +0700

    Поправлен Форд-Беллман

--- Ruban/2_sem/Lab5/Monstruos/F-B.cpp
+++ Ruban/2_sem/Lab5/Monstruos/F-B.cpp
@@ -8,43 +8,8 @@
 
 using namespace std;
 
-int* Ford_Bellman_for_all(int **C, int n, int start);
-bool isStable(int **A, int n);
-
-int* Ford_Bellman_for_all(int **C, int n, int start) {
-	int i, j, k;
-	char x;
-	static int **D = new int *[2];
-	for(i = 0; i < 2; ++i) {
-		D[i] = new int [n];
-		for(j = 0; j < n; ++j)
-			D[i][j] = INF;
-	}
-	D[0][start] = 0;
-	k = 1;
-	int k1;
-	while( !isStable(D, n) ) {
-		k = (++k) % 2;
-		k1 = (k + 1) % 2;
-		for(i = 0; i < n; ++i) {
-			for(j = 0; j < n; ++j) {
-				if( D[k1][i] > D[k][j] + C[j][i] ) 
-					D[k1][i] = D[k][j] + C[j][i];
-			}
-			if(D[k1][i] > LIMIT)
-				D[k1][i] = INF;
-		}
-	}
-	return D[k1];
-}
-
-bool isStable(int **A, int n) {
-	for(int i = 0; i < n; ++i) {
-		if(!(A[0][i] == A[1][i] && A[0][i] <= LIMIT && A[1][i] <= LIMIT))
-			return false;
-	}
-	return true;
-}
+int* Ford_Bellman_for_all(int **C, int n, int start, int *W);
+void print_way(int *W, int n, int start, int end);
 
 int main(int argc, char const *argv[])
 {
@@ -67,20 +32,80 @@ int main(int argc, char const *argv[])
 		}
 	}
 
+	int *W = new int [n];
 	cout << "Print start vertex (from 0 to " << n-1 << ")" << endl;
 	cin >> start;
-
+	for(i = 0; i < n; ++i)
+		W[i] = start;
 	for(i = 0; i < n; ++i) {
 		for(j = 0; j < n; ++j)
 			A[i][j] == INF ? cout << "INF " : cout << A[i][j] << " ";
 		cout << endl;
 	}
 	
-	res = Ford_Bellman_for_all(A, n, start);
+	res = Ford_Bellman_for_all(A, n, start, W);
 	cout << endl << endl;
+	
+	cout << "W = ";
+	for(i = 0; i < n; ++i)
+		cout << " " << W[i];
+	cout << endl;
+	cin >> i;
+
 	for(i = 0; i < n; ++i) {
-		cout << "mass of way from " << start << " to " << i << " is eq to " << res[i] << endl;
+		cout << "mass of way from " << start << " to " << i << " is eq to " << res[i] << ":";
+		print_way(W, n, start, i);
+		cout << endl;
 	}
 	system("pause");
 	return 0;
+}
+
+
+int* Ford_Bellman_for_all(int **C, int n, int start, int *W) {
+	int z ,i, j, k;
+	char x;
+	static int **D = new int *[2];
+	for(i = 0; i < 2; ++i) {
+		D[i] = new int [n];
+		for(j = 0; j < n; ++j)
+			D[i][j] = INF;
+	}
+	D[0][start] = 0;
+
+	k = 1;
+	int k1;
+	bool changed = false;
+	for(z = 0; z < n-1; z++) {
+		k = (++k) % 2;
+		k1 = (k + 1) % 2;
+		for(i = 0; i < n; ++i) {
+			D[k1][i] = D[k][i];
+			for(j = 0; j < n; ++j) {
+				if( D[k1][i] > D[k][j] + C[j][i] ) { 
+					D[k1][i] = D[k][j] + C[j][i];
+					W[i] = j;
+					changed = true;
+				}
+			}
+			if(D[k1][i] > LIMIT)
+				D[k1][i] = INF;
+		}
+		if(!changed)
+			break;
+	}
+	return D[k1];
+}
+
+void print_way(int *W, int n, int start, int end)
+{
+	int a;
+	int i = end;
+	while((i != start)) {
+		cout << i << " <- ";
+		i = W[i];
+		//cin >> a;
+	}
+	cout << i;
+	return;
 }
