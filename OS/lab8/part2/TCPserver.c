1c3e22d0dd2c0dd2248a08fa2e4f5fbdbf76a5e5
Monstruos <mongstruos@mail.ru>
2017-11-28 21:25:32 +0700

    Лаба по ОСям

--- OS/lab8/part2/TCPserver.c
+++ OS/lab8/part2/TCPserver.c
@@ -19,15 +19,26 @@ int main()
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	bind(listener, (struct sockaddr *)&addr, sizeof(addr));
 	
-	listen(listener, 1);
-	
-	sock = accept(listener, NULL, NULL);
+	listen(listener, 3);
+	socklen_t l = sizeof(struct sockaddr_in);
+	sock = accept(listener, (struct sockaddr *)&client, (socklen_t *)&l);
 	
 	recv(sock, buf, 256, 0);
 	
-	char msg[256] = "HTTP /1.0 200 ok\n\n";
+	char msg[256] = "HTTP/1.0 200 OK\r\n\r\n"
+	"<html> \r\n"
+	"<head> \r\n"
+		"Hi \r\n"
+	"</head> \r\n"
+	"<body> \r\n"
+		"Hello everyone!!! \r\n"
+	"</body> \r\n"
+	"</html> \r\n";
+
 	send(sock, msg, 256, 0);
+	recv(sock, buf, 256, 0);
 	
+	// printf("%s", buf);
 	close(sock);
 	return 0;
 }
