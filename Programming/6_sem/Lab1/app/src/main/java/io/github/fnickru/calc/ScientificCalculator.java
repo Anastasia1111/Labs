8a8e351acceecdb2310aa8e23dee49ebf9ab8bc1
Stanislav Mayorov <minigamer97@gmail.com>
2018-02-15 19:38:13 +0700

    Сделал каркас реализации калькулятора
    Определил операции, выполняемые калькулятором, и основной функционал

--- Programming/6_sem/Lab1/app/src/main/java/io/github/fnickru/calc/ScientificCalculator.java
+++ Programming/6_sem/Lab1/app/src/main/java/io/github/fnickru/calc/ScientificCalculator.java
@@ -1,18 +1,62 @@
 package io.github.fnickru.calc;
 
-/** Инженерный калькулятор в виде неленивого сингтона
+/**
+ * Инженерный калькулятор в виде неленивого сингтона
  */
 public enum ScientificCalculator implements Calculator {
 
     INSTANCE;
 
-    boolean isExpressionCorrect = true;
-    String lastResult = "";
-    String lastError = "";
+    /**
+     * Последняя левоассоциативная операция операция, выполненная калькулятором.
+     * <b>Пример:</> для выражения "2+3*7" последней операцией будет "+21".
+     */
+    private String lastOperation = "";
+
+    /**
+     * Список операций, поддерживаемых калькулятором:
+     * <ul>
+     *     <li>+ - сложение</li>
+     *     <li>- - вычитание</li>
+     *     <li>* - умножение</li>
+     *     <li>/ - деление</li>
+     *     <li>% - остаток от деления</li>
+     *     <li>u - унарный минус</>
+     *     <li>s - синус</li>
+     *     <li>c - косинус</li>
+     *     <li>l - двоичный логарифм</li>
+     *     <li>q - квадратный корень</li>
+     * </ul>
+     */
+    private String operators = "+-*/%usclq";
+
+    private String delimiters = operators + "( )";
+    private boolean isExpressionCorrect = true;
+    private String lastResult = "";
+    private String lastError = "";
+
+    private boolean isOperator(String x){
+        if(x.length() != 1) return false;
+        return operators.contains(x);
+    }
+
+    private boolean isDelimiter(String x){
+        if(x.length() != 1) return false;
+        return delimiters.contains(x);
+    }
+
+    private String infix2postfix(String infix) {
+        String postfix = "";
+
+
+
+        return postfix;
+    }
 
     @Override
     public boolean calculate(String expression) {
-        return false;
+        String postfix = infix2postfix(expression);
+        return isExpressionCorrect;
     }
 
     @Override
