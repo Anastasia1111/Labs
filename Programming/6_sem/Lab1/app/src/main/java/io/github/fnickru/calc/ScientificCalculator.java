9808e29deb80f9631d3a0652a16245f5e66a84fa
Stanislav Mayorov <minigamer97@gmail.com>
2018-02-16 21:15:18 +0700

    Сделал транслятор в ОПЗ (Андроид)

--- Programming/6_sem/Lab1/app/src/main/java/io/github/fnickru/calc/ScientificCalculator.java
+++ Programming/6_sem/Lab1/app/src/main/java/io/github/fnickru/calc/ScientificCalculator.java
@@ -1,5 +1,10 @@
 package io.github.fnickru.calc;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * Инженерный калькулятор в виде неленивого сингтона
  */
@@ -21,41 +26,123 @@ public enum ScientificCalculator implements Calculator {
      *     <li>* - умножение</li>
      *     <li>/ - деление</li>
      *     <li>% - остаток от деления</li>
-     *     <li>u - унарный минус</>
-     *     <li>s - синус</li>
-     *     <li>c - косинус</li>
-     *     <li>l - двоичный логарифм</li>
-     *     <li>q - квадратный корень</li>
+     *     <li>u- - унарный минус</>
+     *     <li>sin - синус</li>
+     *     <li>cos - косинус</li>
+     *     <li>log - двоичный логарифм</li>
+     *     <li>sqrt - квадратный корень</li>
      * </ul>
      */
-    private String operators = "+-*/%usclq";
+    private String[] operators = new String[]{
+            "+",
+            "-",
+            "*",
+            "/",
+            "%",
+            "u-",
+            "sin",
+            "cos",
+            "log",
+            "sqrt"
+    };
 
-    private String delimiters = operators + "( )";
+    private String[] separators = ArrayUtils.addAll(operators,"(", " ", ")");
     private boolean isExpressionCorrect = true;
     private String lastResult = "";
     private String lastError = "";
 
     private boolean isOperator(String x){
-        if(x.length() != 1) return false;
-        return operators.contains(x);
+        return Arrays.asList(operators).contains(x);
     }
 
-    private boolean isDelimiter(String x){
-        if(x.length() != 1) return false;
-        return delimiters.contains(x);
+    private boolean isSeparator(String x){
+        return Arrays.asList(separators).contains(x);
+    }
+
+    private int priority(String x) {
+        switch (x) {
+            case "(":
+                return 1;
+            case "+":
+            case "-":
+                return 2;
+            case "*":
+            case "/":
+            case "%":
+                return 3;
+            default:
+                return 4; // Унарные операции
+        }
     }
 
     private String infix2postfix(String infix) {
         String postfix = "";
+        Stack<String> stack = new Stack<>();
 
+        for (String separator : separators) {
+            infix = infix.replace(separator, "~" + separator + "~");
+        }
+        infix = infix.replace("~~", "~");
+        String[] tokens = infix.split("~");
 
+        String prev = "", curr;
+        for (int i = 0; i < tokens.length; ++i)
+        {
+            curr = tokens[i];
+            if (i == (tokens.length - 1) && isOperator(curr)) {
+                postfix = ("#ERR1"); // Некорректное выражение
+                isExpressionCorrect = false;
+                return postfix;
+            }
+            if (curr.equals(" ")) continue;
+            if (isSeparator(curr)) {
+                switch (curr) {
+                    case "(":
+                        stack.push(curr);
+                        break;
+                    case ")":
+                        while (!stack.peek().equals("(")) {
+                            postfix = postfix.concat(stack.pop() + " ");
+                            if (stack.isEmpty()) {
+                                postfix = ("#ERR2"); // Скобки не согласованы
+                                isExpressionCorrect = false;
+                                return postfix;
+                            }
+                        }
+                        stack.pop();
+                        break;
+                    default:
+                        if (curr.equals("-") && (prev.equals("") || (isSeparator(prev)  && !prev.equals(")")))) {
+                            curr = "u-";
+                        } else {
+                            while (!stack.isEmpty() && (priority(curr) <= priority(stack.peek()))) {
+                                postfix = postfix.concat(stack.pop() + " ");
+                            }
+                        }
+                        stack.push(curr);
+                }
+            } else {
+                postfix = postfix.concat(curr + " ");
+            }
+            prev = curr;
+        }
+        while (!stack.isEmpty()) {
+            if (isOperator(stack.peek()))
+                postfix = postfix.concat(stack.pop() + " ");
+            else {
+                postfix = "#ERR2"; // Скобки не согласованы
+                isExpressionCorrect = false;
+                return postfix;
+            }
+        }
 
-        return postfix;
+        return postfix.trim();
     }
 
     @Override
     public boolean calculate(String expression) {
-        String postfix = infix2postfix(expression);
+        //String postfix = infix2postfix(expression);
+        lastResult = infix2postfix(expression);
         return isExpressionCorrect;
     }
 
@@ -67,5 +154,21 @@ public enum ScientificCalculator implements Calculator {
     @Override
     public String result() {
         return lastResult;
+    }
+
+    /**
+     * Функция для тестирования работоспособности. Будет удалена после окончания работы над классом
+     */
+    public static void main(String[] args) {
+        ScientificCalculator calc = ScientificCalculator.INSTANCE;
+
+        calc.calculate("92/10");
+        System.out.println(calc.result());
+        calc.calculate("2+2-2*2/2");
+        System.out.println(calc.result());
+        calc.calculate("(15) * -((3) - (4))");
+        System.out.println(calc.result());
+        calc.calculate("(25+sqrt(4)*5)/10--5%sin(45+90/2)");
+        System.out.println(calc.result());
     }
 }
