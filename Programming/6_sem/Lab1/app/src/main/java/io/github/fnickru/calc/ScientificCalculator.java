114a86695e1774d43af64e6a861e10f570010ee2
Stanislav Mayorov <minigamer97@gmail.com>
2018-02-18 18:23:32 +0700

    Закончил работу над калькулятором (Андроид)
    Возможно, потом в голову придут мелкие правки, но пока что это всё.

--- Programming/6_sem/Lab1/app/src/main/java/io/github/fnickru/calc/ScientificCalculator.java
+++ Programming/6_sem/Lab1/app/src/main/java/io/github/fnickru/calc/ScientificCalculator.java
@@ -37,29 +37,36 @@ public enum ScientificCalculator implements Calculator {
      *     <li>sqrt - квадратный корень</li>
      * </ul>
      */
-    private String[] operators = new String[]{
-            "+",
-            "-",
-            "*",
-            "/",
-            "%",
-            "u-",
-            "sin",
-            "cos",
-            "log",
-            "sqrt"
-    };
+    private String[] binaryOperators = new String[]{"+", "-", "*", "/", "%",};
+    private String[] operators = ArrayUtils.addAll(binaryOperators, "u-", "sin", "cos", "log", "sqrt");
+    private String[] separators = ArrayUtils.addAll(operators, "(", ")");
 
-    private String[] separators = ArrayUtils.addAll(operators,"(", ")");
     private boolean isExpressionCorrect = true;
     private String lastResult = "";
     private String lastError = "";
 
-    private boolean isOperator(String x){
+    private boolean isOperator(String x) {
         return Arrays.asList(operators).contains(x);
     }
 
-    private boolean isSeparator(String x){
+    private boolean isBinaryOperator(String x) {
+        return Arrays.asList(binaryOperators).contains(x);
+    }
+
+    private boolean isUnaryOperator(String x) {
+        return isOperator(x) && !isBinaryOperator(x);
+    }
+
+    private boolean isOperand(String x) {
+        try {
+            Double.valueOf(x);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isSeparator(String x) {
         return Arrays.asList(separators).contains(x);
     }
 
@@ -77,6 +84,26 @@ public enum ScientificCalculator implements Calculator {
             default:
                 return 4; // Унарные операции
         }
+    }
+
+    private String complete(String expression) {
+        String extendedExpression = expression;
+
+        if (extendedExpression.endsWith("=")) {
+            String subexp = extendedExpression.substring(0, extendedExpression.length() - 1);
+            if (isOperand(subexp))
+                extendedExpression = extendedExpression.replace("=", lastOperation);
+            else {
+                String possibleOperator = subexp.substring(subexp.length() - 1);
+                String possibleOperand = subexp.substring(0, subexp.length() - 1);
+                if (isBinaryOperator(possibleOperator) && isOperand(possibleOperand))
+                    extendedExpression = extendedExpression.replace((possibleOperator + "="), (possibleOperator + possibleOperand));
+                else
+                    extendedExpression = subexp;
+            }
+        }
+
+        return extendedExpression;
     }
 
     private String infix2postfix(String infix) {
@@ -147,10 +174,10 @@ public enum ScientificCalculator implements Calculator {
     {
         String[] tokens = expression.split(" ");
         Stack<String> stack = new Stack<>();
+        Double res, arg1, arg2;
 
         try {
             for (String token : tokens) {
-                Double res, arg1, arg2;
                 switch (token) {
                     case "+":
                         arg1 = Double.valueOf(stack.pop());
@@ -222,27 +249,34 @@ public enum ScientificCalculator implements Calculator {
             }
         } catch (NumberFormatException e) {
             isExpressionCorrect = false;
-            lastError = "#NaN";
+            String wrongArgument = e.getMessage().substring(e.getMessage().indexOf("\"")).replace("\"", "");
+            lastError = String.format("#NaN (%s)", wrongArgument);
             return "";
         } catch (ArithmeticException e) {
             isExpressionCorrect = false;
             lastError = "#ZERODIV";
             return "";
         }
-        return String.format(Locale.getDefault(), "%.8f", Double.valueOf(stack.pop())).replace(",", ".");
+        try {
+            res = Double.valueOf(stack.pop());
+            return String.format(Locale.getDefault(), "%.8f", res).replace(",", ".");
+        } catch (NumberFormatException e) {
+            isExpressionCorrect = false;
+            lastError = "#EMPTY";
+            return "";
+        }
     }
 
     @Override
     public boolean calculate(String expression) {
+        String infix = complete(expression);
+
         isExpressionCorrect = true;
         lastOperation = "";
         lastResult = "";
         lastError = "";
 
-        if (expression.equals(lastResult))
-            expression = expression + lastOperation;
-
-        String postfix = infix2postfix(expression);
+        String postfix = infix2postfix(infix);
         if (isExpressionCorrect)
             lastResult = postfixCalc(postfix);
         return isExpressionCorrect;
@@ -264,19 +298,31 @@ public enum ScientificCalculator implements Calculator {
     public static void main(String[] args) {
         ScientificCalculator calc = ScientificCalculator.INSTANCE;
 
-        calc.calculate("92/10");
-        System.out.println(calc.result());
-        calc.calculate(calc.result());
-        System.out.println(calc.result());
+        calc.calculate("");
+        System.out.println(calc.result()); // #EMPTY
+        calc.calculate("50");
+        System.out.println(calc.result()); // 50
+        calc.calculate(calc.result() + "=");
+        System.out.println(calc.result()); // 50
+        calc.calculate("2+");
+        System.out.println(calc.result()); // #WRONGEXP
+        calc.calculate("4+=");
+        System.out.println(calc.result()); // 8
+        calc.calculate("7=");
+        System.out.println(calc.result()); // 11
+        calc.calculate("92/10=");
+        System.out.println(calc.result()); // 9.2
+        calc.calculate(calc.result() + "=");
+        System.out.println(calc.result()); // 0.92
         calc.calculate("2+2-2*2/2");
-        System.out.println(calc.result());
-        calc.calculate(calc.result());
-        System.out.println(calc.result());
-        calc.calculate("15 * -((3) - (4))");
-        System.out.println(calc.result());
-        calc.calculate(calc.result());
-        System.out.println(calc.result());
-        calc.calculate("(25+sqrt(4)*5)/10--5%cos(45+90/2)");
-        System.out.println(calc.result());
+        System.out.println(calc.result()); // 2
+        calc.calculate(calc.result() + "=");
+        System.out.println(calc.result()); // 0
+        calc.calculate("15 * -((3) - (4))=");
+        System.out.println(calc.result()); // 15
+        calc.calculate(calc.result() + "=");
+        System.out.println(calc.result()); // 15
+        calc.calculate("(25+sqrt(4)*5)/10--5%cos(45+90/2)=");
+        System.out.println(calc.result()); // #ZERODIV
     }
 }
