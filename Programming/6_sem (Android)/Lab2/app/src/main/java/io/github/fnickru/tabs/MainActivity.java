542b99133690fbeb569e61f041b8709256fbfd27
Stanislav Mayorov <minigamer97@gmail.com>
2018-03-03 19:49:19 +0700

    Cделал первую вкладку (Андроид)
    Все попытки минимизировать говяность происходящего без кардинальной перестройки кода оказались безуспешны

--- Programming/6_sem (Android)/Lab2/app/src/main/java/io/github/fnickru/tabs/MainActivity.java
+++ Programming/6_sem (Android)/Lab2/app/src/main/java/io/github/fnickru/tabs/MainActivity.java
@@ -3,9 +3,9 @@ package io.github.fnickru.tabs;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TabHost;
 
-@Deprecated
 public class MainActivity extends TabActivity {
 
     @Override
@@ -18,17 +18,17 @@ public class MainActivity extends TabActivity {
         TabHost.TabSpec tabSpec;
 
         tabSpec = tabHost.newTabSpec("tag1");
-        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setIndicator(getResources().getString(R.string.first_tab_name));
         tabSpec.setContent(new Intent(this, FirstActivity.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag2");
-        tabSpec.setIndicator("Вкладка 2");
+        tabSpec.setIndicator(getResources().getString(R.string.second_tab_name));
         tabSpec.setContent(new Intent(this, SecondActivity.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag3");
-        tabSpec.setIndicator("Вкладка 3");
+        tabSpec.setIndicator(getResources().getString(R.string.third_tab_name));
         tabSpec.setContent(new Intent(this, ThirdActivity.class));
         tabHost.addTab(tabSpec);
     }
