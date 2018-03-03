96ca7ea4036aaa6e13f92e1641df27f59be4190d
Monstruos <monstruos@mail.ru>
2018-03-03 23:42:49 +0700

    [Андроид] Сделана 2 лаба
    Сделана только на словах. В идеале, все должно было быть распихано по отдельным активити и красивее.
    Прога в данном состоянии удовлетворяет требованиям к лабе.

--- Programming/6_sem (Android)/Monstruos/Lab2/app/src/main/java/com/example/monstruos/lab2/MainActivity.java
+++ Programming/6_sem (Android)/Monstruos/Lab2/app/src/main/java/com/example/monstruos/lab2/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.monstruos.lab2;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,13 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         TabHost.TabSpec tabSpec;
 
-//        Intent intent1 = new Intent(this, FstActivity.class);
         tabSpec = tabHost.newTabSpec("tag1");
         tabSpec.setIndicator("Вкладка 1");
         tabSpec.setContent(R.id.tab1);
         tabHost.addTab(tabSpec);
 
-//        Intent intent2 = new Intent(this, SecActivity.class);
         ListView list = findViewById(R.id.main_act_listview);
         ArrayAdapter<String> adapt = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
         list.setAdapter(adapt);
@@ -38,26 +41,61 @@ public class MainActivity extends AppCompatActivity {
         tabSpec.setContent(R.id.tab2);
         tabHost.addTab(tabSpec);
 
-//        Intent intent3 = new Intent(this, ThirdActivity.class);
-        TableLayout t = new TableLayout(this);
-        TableRow r = new TableRow(this);
+        TableLayout table = new TableLayout(this);
+        table.setStretchAllColumns(true);
+        table.setShrinkAllColumns(true);
+        table.setLayoutParams(new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
 
-        t.setShrinkAllColumns(true);
-        t.setStretchAllColumns(true);
+        TableRow row0 = new TableRow(this);
+        TableRow.LayoutParams params = new TableRow.LayoutParams();
+        params.span = 4;
+        row0.setLayoutParams(params);
 
         TextView txt = new TextView(this);
-        txt.setText("new");
+        txt.setText("This is TABLE!!!");
+        row0.addView(txt);
+        row0.setGravity(Gravity.CENTER_HORIZONTAL);
 
-        r.addView(txt);
-        r.setGravity(Gravity.CENTER);
+        TableRow row1 = new TableRow(this);
+        TextView id1 = new TextView(this);
+        TextView name1 = new TextView(this);
+        TextView age1 = new TextView(this);
 
-        t.addView(r);
+        id1.setText("1");
+        name1.setText("User Userovich");
+        age1.setText("18");
 
+        row1.addView(id1);
+        row1.addView(name1);
+        row1.addView(age1);
+        row1.setGravity(Gravity.CENTER_HORIZONTAL);
 
+        TableRow row2 = new TableRow(this);
+        TextView id2 = new TextView(this);
+        TextView name2 = new TextView(this);
+        TextView age2 = new TextView(this);
+
+        id2.setText("2");
+        name2.setText("Moder Moderovich");
+        age2.setText("99");
+
+        row2.addView(id2);
+        row2.addView(name2);
+        row2.addView(age2);
+        row2.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        table.addView(row0);
+        table.addView(row1);
+        table.addView(row2);
+
+        LinearLayout Tab3 = findViewById(R.id.tab3);
+        Tab3.addView(table);
 
         tabSpec = tabHost.newTabSpec("tag3");
         tabSpec.setIndicator("Вкладка 3");
-        tabSpec.setContent();
+        tabSpec.setContent(R.id.tab3);
         tabHost.addTab(tabSpec);
 
         tabHost.setCurrentTabByTag("tag2");
