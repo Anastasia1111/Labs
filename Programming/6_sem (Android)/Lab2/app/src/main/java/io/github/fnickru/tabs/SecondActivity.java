9b7932cf6323fcf3497eb1b7e20795b652e51437
Stanislav Mayorov <minigamer97@gmail.com>
2018-03-04 18:34:39 +0700

    Формально сделал лабу №2 (Андроид)
    И даже исправил дизайн

--- Programming/6_sem (Android)/Lab2/app/src/main/java/io/github/fnickru/tabs/SecondActivity.java
+++ Programming/6_sem (Android)/Lab2/app/src/main/java/io/github/fnickru/tabs/SecondActivity.java
@@ -2,6 +2,8 @@ package io.github.fnickru.tabs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sec);
+
+        ListView list = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapt = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{
+                "First string", "Вторая строка", "Troisième ligne", "Quarta acie"
+        });
+        list.setAdapter(adapt);
     }
 }
--- Programming/6_sem (Android)/Lab2/app/src/main/java/io/github/fnickru/tabs/ThirdActivity.java
+++ Programming/6_sem (Android)/Lab2/app/src/main/java/io/github/fnickru/tabs/ThirdActivity.java
@@ -2,6 +2,10 @@ package io.github.fnickru.tabs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +13,59 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        TableLayout table = new TableLayout(this);
+
+        table.setShrinkAllColumns(true);
+        table.setStretchAllColumns(true);
+        table.setLayoutParams(new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                TableLayout.LayoutParams.WRAP_CONTENT));
+
+        TableRow[] row = new TableRow[3];
+        TextView[] id = new TextView[3];
+        TextView[] name = new TextView[3];
+        TextView[] age = new TextView[3];
+
+        for (int i = 0; i < 3; ++i)
+        {
+            row[i] = new TableRow(this);
+            id[i] = new TextView(this);
+            name[i] = new TextView(this);
+            age[i] = new TextView(this);
+        }
+
+        id[0].setText("id");
+        name[0].setText("Name");
+        age[0].setText("Age");
+
+        row[0].addView(id[0]);
+        row[0].addView(name[0]);
+        row[0].addView(age[0]);
+        row[0].setGravity(Gravity.CENTER_HORIZONTAL);
+
+        id[1].setText("1");
+        name[1].setText("John Smith");
+        age[1].setText("24");
+
+        row[1].addView(id[1]);
+        row[1].addView(name[1]);
+        row[1].addView(age[1]);
+        row[1].setGravity(Gravity.CENTER_HORIZONTAL);
+
+        id[2].setText("2");
+        name[2].setText("Smith John");
+        age[2].setText("42");
+
+        row[2].addView(id[2]);
+        row[2].addView(name[2]);
+        row[2].addView(age[2]);
+        row[2].setGravity(Gravity.CENTER_HORIZONTAL);
+
+        table.addView(row[0]);
+        table.addView(row[1]);
+        table.addView(row[2]);
+
+        setContentView(table);
     }
 }
