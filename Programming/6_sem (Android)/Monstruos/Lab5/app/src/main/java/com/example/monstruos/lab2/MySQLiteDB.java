41c52525cd47bc0bf869953ffa873c0ba3c78590
Monstruos <monstruos@mail.ru>
2018-04-07 20:02:52 +0700

    Сделал лабу по андроиду
    Как и прежде, она выполняет исключительно те требования, которые надо, ничего сверх того, увы.

--- Programming/6_sem (Android)/Monstruos/Lab5/app/src/main/java/com/example/monstruos/lab2/MySQLiteDB.java
+++ Programming/6_sem (Android)/Monstruos/Lab5/app/src/main/java/com/example/monstruos/lab2/MySQLiteDB.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.Random;
+
 public class MySQLiteDB extends SQLiteOpenHelper {
 
     public static final String TABLE_NAME = "StudTable";
@@ -19,9 +21,25 @@ public class MySQLiteDB extends SQLiteOpenHelper {
                 "weight INTEGER, " +
                 "height INTEGER, " +
                 "age INTEGER );");
+
+        for(int i = 0; i < 50; ++i) {
+            Random rand = new Random();
+            Integer randInt = rand.nextInt(5)  + i * 5;
+            String st_name = "st" + randInt.toString();
+            randInt = rand.nextInt(7) + 18;
+            String st_age = randInt.toString();
+            randInt = rand.nextInt(40) + 70;
+            String st_weight = randInt.toString();
+            randInt = rand.nextInt(100) + 140;
+            String st_height = randInt.toString();
+            db.execSQL("INSERT INTO StudTable ('name', 'weight', 'height', 'age') " +
+                    "VALUES ('" + st_name + "', '" + st_weight + "', '" +
+                    st_height + "', '" + st_age + "')");
+
+        }
     }
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
-        database.execSQL("DROP TABLE IS EXISTS " + TABLE_NAME);
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         database.execSQL("CREATE TABLE StudTable (" +
                 "_id INTEGER PRIMARY KEY autoincrement, " +
                 "name TEXT, " +
